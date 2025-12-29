a4420660ae7985657189046f46fad5da6cfec760
denissoboslai13 <soboslaid@gmail.com>
2025-12-29 09:47:02 +0100

    works?

--- backend-server/models/note.js
+++ backend-server/models/note.js
@@ -5,17 +5,21 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-
-  .then(result => {
+mongoose
+  .connect(url)
+  .then((result) => {
     console.log('connected to MongoDB')
   })
-  .catch(error => {
+  .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   important: Boolean,
 })
 
@@ -24,7 +28,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
 module.exports = mongoose.model('Note', noteSchema)
